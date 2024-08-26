81ea03155ce3d3caed5931a5a65b5a03215e0504
nimamovic <nimamovic3@etf.unsa.ba>
2024-08-26 22:54:31 +0200

    Simple app for getting one, all, or adding new item

---
+++ handler.js
@@ -0,0 +1,79 @@
+const AWS = require('aws-sdk');
+const { v4: uuidv4 } = require('uuid');
+
+AWS.config.update({
+  region: 'eu-north-1'
+});
+
+const dynamoDBdocumentClient = new AWS.DynamoDB.DocumentClient();
+const tableName = 'Items';
+
+module.exports.getItems = async () => {
+  const params = {
+    TableName
+  };
+
+  try {
+    const data = await dynamoDBdocumentClient.scan(params).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data.Items)
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Error while fatching items', details: err })
+    };
+  }
+};
+
+module.exports.getItem = async (event) => {
+  const { id } = event.pathParameters;
+  const params = {
+    TableName,
+    Key: { id }
+  };
+
+  try {
+    const data = await dynamoDBdocumentClient.get(params).promise();
+    if (!data.Item) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ message: 'Item not found' })
+      };
+    }
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data.Item)
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Error while fatching item with id: ' + id, details: err })
+    };
+  }
+};
+
+module.exports.createItem = async (event) => {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName,
+    Item: {
+      id: uuidv4(),
+      name: data.name
+    }
+  };
+
+  try {
+    await dynamoDBdocumentClient.put(params).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Item created', item: params.Item })
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Error while creating item.', details: err })
+    };
+  }
+};
