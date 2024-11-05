27ead5a1a074a1b552298113245385ad75ea9590
Nasiha Imamovic <Nasiha.Imamovic@endava.com>
2024-11-05 13:32:07 +0100

    new testing of secrets

--- handler.js
+++ handler.js
@@ -13,6 +13,8 @@ module.exports.getItems = async () => {
     TableName: tableName
   };
 
+  const nasa = "nasa";
+
   try {
     const data = await dynamoDBdocumentClient.scan(params).promise();
     return {
